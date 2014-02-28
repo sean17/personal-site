156e2416355819b5367b0141c4b572a8a4586457
Sean Zawicki <szawicki@blackpearlsystems.com>
2014-02-27 23:44:37 -0800

    Added an experiment page for backgrounds to work with Eduardo on

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var express = require('express'),
-	path	= require('path')
+	path	= require('path'),
+	app		= express(),
+	port	= 80
 	;
-var app = express();
-
 
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
+if(process.argv[2]==='dev')
+{
+	port = 8080;
+	app.get('/new/index', function(req, res, next)
+	{
+		res.render('new/index');
+	});
+}
+
 // app.render('howdy');
 app.get('/', function(req, res, next)
 {
 	res.render('index');
 });
-app.listen(80);
+app.get('/experiments/background', function(req, res, next)
+{
+	res.render('experiments/background');
+});
+app.listen(port);
